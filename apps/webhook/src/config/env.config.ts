0f28bddbedd6f08ca864370de35397c4519c8a16
Sokratis Vidros <sokratis.vidros@gmail.com>
2025-02-17 16:46:11 +0200

    chore(root): Remove all V1 related services and libs from next

--- apps/webhook/src/config/env.config.ts
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv';
-import path from 'node:path';
-import { getContextPath, NovuComponentEnum, getEnvFileNameForNodeEnv } from '@novu/shared';
-
-dotenv.config({ path: path.join(__dirname, '..', getEnvFileNameForNodeEnv(process.env.NODE_ENV)) });
-
-export const CONTEXT_PATH = getContextPath(NovuComponentEnum.WEBHOOK);
